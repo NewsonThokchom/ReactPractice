a06469d487b74ca42e569694945d16fc11dd06a4
NewsonThokchom <43926860+NewsonThokchom@users.noreply.github.com>
2021-07-12 00:01:46 +0530

    implemented click event

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,17 @@
 const Home = () => {
+  const handleClick = (e) => {
+    console.log("Hello Newson ", e);
+  };
+  const handleClickAgain = (name, e) => {
+    console.log("Hello " + name, e.target);
+  };
   return (
     <div className="home">
       <h2>Home Component</h2>
+      <button onClick={handleClick}>Click me</button>
+      <button onClick={(e) => handleClickAgain("Newson", e)}>
+        Click me again
+      </button>
     </div>
   );
 };
