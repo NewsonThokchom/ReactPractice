d052ffe04f81c97eb465f437b7fd1fb159ad11c1
NewsonThokchom <43926860+NewsonThokchom@users.noreply.github.com>
2021-07-12 09:55:45 +0530

    BlogList Component added and implemented props

--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,21 @@
 import { useState } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
-  //   let name = "mario";
-  const [name, setName] = useState("mario");
-  const [age, setAge] = useState(25);
-  const handleClick = () => {
-    setName("Luigi");
-    setAge(30);
-  };
+  const [blogs, setBlogs] = useState([
+    { title: "My new website", body: "Lorem ipsum..", author: "mario", id: 1 },
+    { title: "Welcome party!", body: "Lorem ipsum..", author: "yoshi", id: 2 },
+    {
+      title: "Web dev top tips",
+      body: "Lorem ipsum..",
+      author: "mario",
+      id: 3,
+    },
+  ]);
 
   return (
     <div className="home">
-      <h2>Home Component</h2>
-      <p>
-        {name} is {age} old.
-      </p>
-      <button onClick={handleClick}>Click me</button>
+      <BlogList blogs={blogs} title="All Blogs" />
     </div>
   );
 };
