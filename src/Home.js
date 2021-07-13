08b3319a33c715863e597e67a37a15297d79a7dc
NewsonThokchom <43926860+NewsonThokchom@users.noreply.github.com>
2021-07-13 08:11:36 +0530

    Implemented useEffect inside HomeComponent

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
@@ -13,13 +13,25 @@ const Home = () => {
     },
   ]);
 
+  const [name, setName] = useState("mario");
+
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter((blog) => blog.id !== id);
+    setBlogs(newBlogs);
+  };
+
+  useEffect(() => {
+    console.log("useEffect ran");
+    console.log(blogs);
+    console.log(name);
+  }, [name]);
+
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs" />
-      <BlogList
-        blogs={blogs.filter((blog) => blog.author === "mario")}
-        title="Mario's Blogs"
-      />
+      <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+      <button onClick={() => setName("Luigi")}>change name</button>
+
+      <p>{name}</p>
     </div>
   );
 };
